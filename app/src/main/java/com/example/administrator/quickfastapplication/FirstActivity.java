245aa252c093197648673404b067ba88680088d5
zpetter <5589728zpetter>
2015-06-24 15:38:43 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/quickfastapplication/FirstActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.quickfastapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZJF on 2015/6/24.
+ */
+public class FirstActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
