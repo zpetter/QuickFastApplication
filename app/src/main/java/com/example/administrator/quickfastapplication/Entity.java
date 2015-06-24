a4abe338265c21f9ee960a1d2e1486e5ff15e079
zpetter <5589728zpetter>
2015-06-24 19:38:08 +0800

    volley +Gson

---
+++ app/src/main/java/com/example/administrator/quickfastapplication/Entity.java
@@ -0,0 +1,8 @@
+package com.example.administrator.quickfastapplication;
+
+/**
+ * Created by ZJF on 2015/6/24.
+ */
+public class Entity  {
+    String a;
+}
--- app/src/main/java/com/example/administrator/quickfastapplication/FirstActivity.java
+++ app/src/main/java/com/example/administrator/quickfastapplication/FirstActivity.java
@@ -1,12 +1,30 @@
 package com.example.administrator.quickfastapplication;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by ZJF on 2015/6/24.
  */
-public class FirstActivity extends Activity{
+public class FirstActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +32,59 @@ public class FirstActivity extends Activity{
         //测试
         //我在这
         //测试2
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String JSONDataUrl = "http://pipes.yahooapis.com/pipes/pipe.run?_id=giWz8Vc33BG6rQEQo_NLYQ&_render=json";
+        final ProgressDialog progressDialog = ProgressDialog.show(this, "This is title", "...Loading...");
+        StringRequest stringRequest = new StringRequest("http://www.baidu.com",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("TAG", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+//        StringRequest stringRequest1 = new StringRequest(Request.Method.POST, url,  listener, errorListener) {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> map = new HashMap<String, String>();
+//                map.put("params1", "value1");
+//                map.put("params2", "value2");
+//                return map;
+//            }
+//        };
+        GsonRequest<Entity> gsonRequest = new GsonRequest<Entity>(JSONDataUrl, Entity.class,
+                new Response.Listener<Entity>() {
+
+                    @Override
+                    public void onResponse(Entity weather) {
+                        // TODO Auto-generated method stub
+//                        WeatherInfo weatherInfo = weather.getWeatherinfo();
+//                        text.setText(text.getText() + weatherInfo.getTime());
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest("http://m.weather.com.cn/data/101010100.html", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
 }
